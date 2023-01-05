21ba4da97bc509334267270b1bcfca994a8a6013
Caio Pedroso <caiofapedroso@gmail.com>
2023-01-05 17:24:49 -0300

    Fix minVideoDuration for fixed viewer

--- example/lib/trimmer_view.dart
+++ example/lib/trimmer_view.dart
@@ -88,6 +88,7 @@ class _TrimmerViewState extends State<TrimmerView> {
                     child: TrimViewer(
                       trimmer: _trimmer,
                       viewerHeight: 50.0,
+                      type: ViewerType.fixed,
                       viewerWidth: MediaQuery.of(context).size.width,
                       durationStyle: DurationStyle.FORMAT_MM_SS,
                       maxVideoLength: const Duration(seconds: 10),
